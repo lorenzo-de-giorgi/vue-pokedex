32daaa9fd4d1985de8a563da7a52ca4f24dea19a
lorenzo-de-giorgi <lorenzodegiorgi2004@gmail.com>
2024-09-10 17:07:29 +0200

    called function "fetchPokemon" in input and button

--- src/components/SearchComponent.vue
+++ src/components/SearchComponent.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
-        <input v-model="pokemonName" placeholder="Inserisci il nome del pokemon" @keyup.enter="">
-        <button @click="">Cerca</button>
+        <input v-model="pokemonName" placeholder="Inserisci il nome del pokemon" @keyup.enter="fetchPokemon">
+        <button @click="fetchPokemon">Cerca</button>
     </div>
 </template>
 
@@ -21,7 +21,7 @@
                     const response = axios.get(`https://pokeapi.co/api/v2/pokemon/${name}`);
                     console.log(response.data);
                 } catch(err){
-                    
+
                 }
             }
         },
