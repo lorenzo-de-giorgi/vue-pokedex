d464d7fba2d2d36ee5e3f62327409cf9ab639bfc
lorenzo-de-giorgi <lorenzodegiorgi2004@gmail.com>
2024-09-11 12:11:47 +0200

    removed SearchComponent from App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,15 @@
 <template>
   <div>
     <PokedexComponent />
-    <!-- <SearchComponent /> -->
   </div>
 </template>
 
 <script>
 import PokedexComponent from './components/PokedexComponent.vue';
-import SearchComponent from './components/SearchComponent.vue';
 
   export default {
     name: 'App',
     components: {
-      SearchComponent,
       PokedexComponent
     },
   }
