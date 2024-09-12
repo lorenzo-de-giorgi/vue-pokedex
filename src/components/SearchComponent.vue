b7b2167e344582ce106154dc368eccf7b0caad2a
lorenzo-de-giorgi <lorenzodegiorgi2004@gmail.com>
2024-09-12 16:04:40 +0200

    removed info from searchComponent

--- src/components/SearchComponent.vue
+++ src/components/SearchComponent.vue
@@ -3,15 +3,6 @@
         <input v-model="pokemonName" placeholder="Inserisci il nome del pokemon" @keyup.enter="fetchPokemon">
         <button @click="fetchPokemon"><i class="fa-solid fa-magnifying-glass"></i> | Cerca</button>
     </div>
-
-    <div v-if="pokemon">
-        <h2>{{ pokemon.name}}</h2>
-        <img :src="pokemon.sprites.front_default" alt="Pokémon Image" />
-        <p><strong>Height:</strong> {{ pokemon.height }}</p>
-        <p><strong>Weight:</strong> {{ pokemon.weight }}</p>
-        <p><strong>Base Experience:</strong> {{ pokemon.base_experience }}</p>
-    </div>
-
     <div v-if="error" class="error">{{ error }}</div>
 </template>
 
