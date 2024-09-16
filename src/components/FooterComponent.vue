537c6edb5fdaa535c9b13e444dad27d5d4a3b693
lorenzo-de-giorgi <lorenzodegiorgi2004@gmail.com>
2024-09-16 17:44:29 +0200

    added a p in footerComponent

---
+++ src/components/FooterComponent.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <p class="text-center">Used <a href="https://pokeapi.co/">PokeAPI</a></p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'FooterComponent'
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
